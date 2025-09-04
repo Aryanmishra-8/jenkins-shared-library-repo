ae336dd4a78b656828af68b74ac6e50ceedacc82
Aryanmishra-8 <aryanmishra9017@gmail.com>
2025-09-04 11:51:37 +0530

    Update gitUtils.groovy

--- vars/gitUtils.groovy
+++ vars/gitUtils.groovy
@@ -1,9 +1,14 @@
 def checkoutRepo(String repoUrl, String branch, String credentialsId) {
-    checkout([$class: 'GitSCM',
-        branches: [[name: branch]],
-        userRemoteConfigs: [[
-            url: repoUrl,
-            credentialsId: credentialsId
-        ]]
-    ])
+    try {
+        checkout([$class: 'GitSCM',
+            branches: [[name: branch]],
+            userRemoteConfigs: [[
+                url: repoUrl,
+                credentialsId: credentialsId
+            ]]
+        ])
+        echo " Repository checkout successful: ${repoUrl} [${branch}]"
+    } catch (Exception e) {
+        error " Git Checkout Failed! Check if repo URL, branch, or credentials are correct.\nError: ${e.message}"
+    }
 }
